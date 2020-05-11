ba33304c4aa3efacb10fedd3dbd7a9f49b36af02
Immo Schuetz <ischtz@posteo.eu>
2020-05-11 15:25:04 +0200

    Add simple eyeball model

--- vzgazetoolbox.py
+++ vzgazetoolbox.py
@@ -159,3 +159,60 @@ class VzGazeRecorder():
 					ef.write(EROW.format(*ev))
 					n += 1
 			self._dlog('Saved {:d} events to file: {:s}'.format(n, event_file))
+
+
+class Eyeball(viz.VizNode):
+	""" Simple eyeball object to visually indicate gaze direction """
+	
+	def __init__(self, radius=0.024, eyecolor='blue', pointer=False, gaze_length=100):
+		""" Initialize a new Eyeball node
+		
+		Args:
+			radius (float): Eyeball radius in m
+			eyecolor (str, 3-tuple): Iris color for this eye (see setEyeColor)
+			pointer (bool): if True, start out the gaze pointer visible
+			gaze_length (float): length of gaze pointer in m
+		"""
+
+		self.eyecolors = {'brown': [0.387, 0.305, 0.203],
+						  'blue':  [0.179, 0.324, 0.433],
+						  'green': [0.109, 0.469, 0.277],
+						  'grey':  [0.285, 0.461, 0.395]}
+		
+		eye = viz.addChild('unit_eye.gltf')
+		viz.VizNode.__init__(self, eye.id)
+		
+		# Add gaze direction pointer (invisible by default)
+		self.pointer = vizshape.addCylinder(height=gaze_length, radius=0.05,
+												 axis=vizshape.AXIS_Z, parent=eye)
+		self.pointer.setPosition([0.0, 0.0, gaze_length/2])
+		if not pointer:
+			self.pointer.visible(viz.OFF)
+
+		# Set a specific eye color (e.g. for disambiguation)
+		eye.setScale([radius/2,] * 3)		
+		self.setEyeColor(eyecolor)
+		
+		
+	def setEyeColor(self, color):
+		""" Set iris color of this eyeball object
+		
+		Args:
+			color : RGB 3-tuple, or one of 'brown', 'blue', 'green', 'grey'
+		"""
+		if type(color) == str:
+			ec = self.eyecolors[color]
+		else:
+			ec = color
+
+		self.getChild('Iris').color(ec)
+		self.pointer.color(ec)
+
+
+	def setGazePointer(self, visible=True):
+		""" Show or hide the gaze pointer on the eye's Z axis
+		
+		Args:
+			visible (bool): if True, draw gaze direction pointer
+		"""
+		self.pointer.visible(visible)
