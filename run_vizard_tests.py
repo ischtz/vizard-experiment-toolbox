4a3263c32e46d14c71ac8dfbf98244ff2eadbf5d
Immo Schuetz <ischtz@posteo.eu>
2021-03-10 15:01:42 +0100

    test: Full factorial design, trial summary

---
+++ run_vizard_tests.py
@@ -0,0 +1,18 @@
+﻿import unittest
+
+import tests.test_experiment as test_experiment
+import tests.test_data as test_data
+
+print('Running unit tests from within Vizard environment...')
+
+print('experiment')
+unittest.main(module=test_experiment, exit=False)
+
+print('data')
+unittest.main(module=test_data)
+
+print('recorder')
+unittest.main(module=test_recorder)
+
+print('replay')
+unittest.main(module=test_replay)
--- tests/test_experiment.py
+++ tests/test_experiment.py
@@ -48,27 +48,27 @@ class TestExperiment(unittest.TestCase):
 
         # Config from dict
         test_config_dict = {'param1': 123.5, 'param2': False}
-        e = Experiment(config=test_config_dict)
+        e = Experiment(name='unittest', config=test_config_dict)
         self.assertEqual(e.config.param1, 123.5)
         self.assertFalse(e.config.param2)
 
         # Config from file
-        e2 = Experiment(config=dummy_json)
+        e2 = Experiment(name='unittest', config=dummy_json)
         self.assertEqual(e2.config.param1, 123.5)
         self.assertFalse(e2.config.param2)
 
         # Config from nonexistant file
         with self.assertRaises(IOError):
-            e = Experiment(config='NOT_A_REAL_CONFIG_FILE.dummy')
+            e = Experiment(name='unittest', config='NOT_A_REAL_CONFIG_FILE.dummy')
 
         # Config from existing but non-JSON file
         with self.assertRaises(ValueError):
-            e = Experiment(config=dummy_csv)
+            e = Experiment(name='unittest', config=dummy_csv)
 
 
     def test_add_trials(self):
 
-        e = Experiment()
+        e = Experiment(name='unittest')
         self.assertEqual(len(e), 0)
 
         # Trial with empty params
@@ -85,8 +85,20 @@ class TestExperiment(unittest.TestCase):
         self.assertEqual(e.trials[1].params.param1, 1234)
         self.assertEqual(e.trials[1].params['param2'], False)
 
+        # List params - correct length
+        e.addTrials(3, list_params={'param3': [1, 2, 3]})
+        self.assertEqual(len(e), 6)
+        self.assertEqual(len(e.trials), 6)
+        self.assertEqual(e.trials[3].params.param3, 1)
+        self.assertEqual(e.trials[4].params.param3, 2)
+        self.assertEqual(e.trials[5].params.param3, 3)
+        
+        # List params - wrong length causes exception
+        with self.assertRaises(ValueError):
+            e.addTrials(3, list_params={'param3': [1, 2, 3, 4]})
+
         # Blocks
-        e2 = Experiment()
+        e2 = Experiment(name='unittest')
         e2.addTrials(1, block=2)
         e2.addTrials(1, block=1)
         self.assertEqual(e2._blocks, [1, 2])
@@ -105,7 +117,7 @@ class TestExperiment(unittest.TestCase):
                       {'param5': 42, 'param4': 'True',  'param3': 200000.0, 'param2': 'penguin','param1': 4}]
 
         # All trials in one block
-        e = Experiment()
+        e = Experiment(name='unittest')
         e.addTrialsFromCSV(file_name=dummy_csv, sep='\t')
 
         self.assertEqual(len(e.trials), 4)
@@ -115,7 +127,7 @@ class TestExperiment(unittest.TestCase):
             self.assertEqual(t.block, e.blocks[0])
 
         # With block column
-        e2 = Experiment()
+        e2 = Experiment(name='unittest')
         e2.addTrialsFromCSV(file_name=dummy_csv, sep='\t', block_col='param1')
         self.assertEqual(len(e2.trials), 4)
         self.assertEqual(e2.blocks, [1, 2, 3, 4])
@@ -129,12 +141,46 @@ class TestExperiment(unittest.TestCase):
             e2.addTrialsFromCSV(file_name=dummy_csv, sep='\t', block_col='**invalid**')
         
         # With block argument overriding column name
-        e3 = Experiment()
+        e3 = Experiment(name='unittest')
         e3.addTrialsFromCSV(file_name=dummy_csv, sep='\t', block_col='param1', block=42)
         self.assertEqual(len(e2.trials), 4)
         self.assertEqual(e3.blocks, [42])
         for t in e3.trials:
             self.assertEqual(t.block, 42)
+
+        # With params dict
+        e4 = Experiment(name='unittest')
+        test_params = {'param1': 1234, 'param2': False}
+        e4.addTrials(2, params=test_params)
+        self.assertEqual(len(e4), 2)
+        self.assertEqual(e4.trials[0].params.param1, 1234)
+        self.assertEqual(e4.trials[0].params['param2'], False)
+
+
+    def test_add_trials_full_factorial(self):
+
+        test_levels = {'a': 5, 'b': 3, 'c': 2}
+        test_labels = {'d': ['test1', 'test2'], 'e': [1, 5, 7, 9]}
+
+        # Factor levels
+        e = Experiment(name='unittest')
+        e.addTrialsFullFactorial(levels=test_levels)
+        self.assertEqual(len(e), 30)
+
+        # Factor levels with repetition
+        e = Experiment(name='unittest')
+        e.addTrialsFullFactorial(levels=test_levels, repeat=2)
+        self.assertEqual(len(e), 60)
+
+        # Factor levels not int
+        e = Experiment(name='unittest')
+        with self.assertRaises(TypeError):
+            e.addTrialsFullFactorial(levels=test_levels, repeat=2.5)
+
+        # Factor labels
+        e = Experiment(name='unittest')
+        e.addTrialsFullFactorial(levels=test_labels)
+        self.assertEqual(len(e), 8)
 
 
     def test_save_trial_data_to_csv(self):
@@ -145,7 +191,7 @@ class TestExperiment(unittest.TestCase):
                   {'param5': '', 'param4': 'False', 'param3': -4,       'param2': 'mouse',  'param1': 3},
                   {'param5': 42, 'param4': 'True',  'param3': 200000.0, 'param2': 'penguin','param1': 4}]
 
-        e = Experiment()
+        e = Experiment(name='unittest')
         e.addTrials(1, params=params[0])
         e.addTrials(1, params=params[1])
         e.addTrials(1, params=params[2])
@@ -155,7 +201,7 @@ class TestExperiment(unittest.TestCase):
         e.saveTrialDataToCSV(file_name=tmp_csv, sep='\t')
 
         # Re-read temp file and compare
-        e2 = Experiment()
+        e2 = Experiment(name='unittest')
         e2.addTrialsFromCSV(file_name=tmp_csv, sep='\t')
         for tidx, t in enumerate(e.trials):
             params_written = dict(t.params)
@@ -167,14 +213,14 @@ class TestExperiment(unittest.TestCase):
 
     def test_trial_start_end(self):
 
-        e = Experiment()
+        e = Experiment(name='unittest')
         e.addTrials(10)
 
         # End trial before start
         self.assertRaises(RuntimeError, e.endCurrentTrial)
 
         # Start and end normally
-        e.startNextTrial()
+        e.startNextTrial(print_summary=False)
         self.assertEqual(e._trial_running, True)
         self.assertIsInstance(e.currentTrial, Trial)
         self.assertEqual(e.currentTrial, e.trials[0])
@@ -182,12 +228,8 @@ class TestExperiment(unittest.TestCase):
         self.assertFalse(e.currentTrial.done)
 
         last_trial = e.currentTrial
-        e.endCurrentTrial()
+        e.endCurrentTrial(print_summary=False)
         self.assertEqual(e._trial_running, False)
-        with self.assertRaises(RuntimeError):
-            _ = e.currentTrial
-        with self.assertRaises(RuntimeError):
-            _ = e.currentTrialNumber
         self.assertFalse(last_trial.running)
         self.assertTrue(last_trial.done)
 
@@ -197,27 +239,27 @@ class TestExperiment(unittest.TestCase):
         # Restart a specific trial
         self.assertRaises(RuntimeError, e.startTrial, trial_idx=0)
         try:
-            e.startTrial(trial_idx=0, repeat=True)
+            e.startTrial(trial_idx=0, repeat=True, print_summary=False)
         except RuntimeError:
             self.fail('Repeating trial caused exception.')
 
         # Start a trial object directly
-        e2 = Experiment()
+        e2 = Experiment(name='unittest')
         e2.addTrials(2)
-        e2.startTrial(e2.trials[0])
-        e2.endCurrentTrial()
+        e2.startTrial(e2.trials[0], print_summary=False)
+        e2.endCurrentTrial(print_summary=False)
 
 
 
     def test_experiment_while_loop(self):
 
-        e = Experiment()
+        e = Experiment(name='unittest')
         e.addTrials(10)
 
         while not e.done:
-            e.startNextTrial()
+            e.startNextTrial(print_summary=False)
             e.currentTrial.results.aaa = 1
-            e.endCurrentTrial()
+            e.endCurrentTrial(print_summary=False)
         
         # Automatically mark experiment done after last trial
         self.assertTrue(e.done)
@@ -229,13 +271,13 @@ class TestExperiment(unittest.TestCase):
 
     def test_experiment_iter_loop(self):
 
-        e = Experiment()
+        e = Experiment(name='unittest')
         e.addTrials(10)
 
         for trial in e:
-            e.startTrial(trial)
+            e.startTrial(trial, print_summary=False)
             trial.results.aaa = 1
-            e.endCurrentTrial()
+            e.endCurrentTrial(print_summary=False)
 
         self.assertTrue(e.done)
         
@@ -246,13 +288,13 @@ class TestExperiment(unittest.TestCase):
 
     def test_experiment_index_loop(self):
 
-        e = Experiment()
+        e = Experiment(name='unittest')
         e.addTrials(10)
 
         for trial_no in range(0, 10):
-            e.startTrial(trial_no)
+            e.startTrial(trial_no, print_summary=False)
             e.trials[trial_no].results['aaa'] = 1
-            e.endCurrentTrial()
+            e.endCurrentTrial(print_summary=False)
 
         self.assertTrue(e.done)
         
