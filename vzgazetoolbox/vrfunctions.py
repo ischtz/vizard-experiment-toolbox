885dcf72b6f227e9de0697119424cf0321005b7f
Immo Schuetz <ischtz@posteo.eu>
2021-04-30 11:35:37 +0200

    Add ObjectCollection class

--- vzgazetoolbox/vrfunctions.py
+++ vzgazetoolbox/vrfunctions.py
@@ -3,9 +3,162 @@
 # Vizard gaze tracking toolbox
 # VR helper functions
 
+import os
+import glob
+
 import viz
+import vizfx
 import vizact
 import viztask
+
+
+class ObjectCollection(dict):
+    """ Holds multiple node objects, similar to a Unity tag. 
+    Each node can belong to more than one ObjectCollection. """
+    def __init__(self, _src=None):
+        self._o = {}
+        if _src is not None:
+            self._o = _src.copy()
+        self._next_idx = 0
+
+
+    def __getitem__(self, key):
+        return self._o[key]
+
+
+    def __setitem__(self, key, value):
+        if not isinstance(value, viz.VizNode):
+            raise TypeError('Can only assign Vizard node objects directly!')
+        else:
+            self._o[key] = value
+
+
+    def __getattr__(self, name):
+        return self._o[name]
+
+
+    def __len__(self):
+        return len(self._o)
+    
+    
+    def __iter__(self):
+        """ Iterates over all node objects in collection.
+        
+        This enables code like:
+        for node in collection:
+            print(node.getPosition())
+        
+        """
+        return iter(self._o.values())
+
+
+    def __contains__(self, node):
+        """ Checks if a given node object is part of this collection. 
+        This does NOT compare keys as would be the case for a dict! """
+        if node in self._o.values():
+            return True
+        else:
+            return False
+
+
+    def __repr__(self):
+        return repr(self._o)
+    
+    
+    def add(self, node, key=None):
+        """ Add a node object to the collection. If no key is specified, 
+        the next available numeric key is used instead
+
+        Args:
+            node: Vizard node3d object """
+        if key is None:
+            # Find a free numerical key
+            key = self._next_idx
+            while key in self._o.keys():
+                key += 1
+            self._next_idx = key + 1
+        self[key] = node
+
+
+    def hideAll(self):
+        """ Set all objects in this collection to invisible """
+        for obj in self._o.keys():
+            self._o[obj].visible(False)
+
+
+    def showAll(self):
+        """ Set all objects in this collection to visible """
+        for obj in self._o.keys():
+            self._o[obj].visible(True)
+
+
+    def hide(self, key):
+        """ Convenience function to set a single object invisible """
+        self._O[key].visible(False)
+
+
+    def show(self, key, position=None, euler=None, scale=None, color=None):
+        """ Set a specific object from the collection to visible. Allows 
+        quickly setting position, scale, euler orientation and/or color
+        in the same call.
+
+        Args:
+            key (str): Key of the object to show
+            position, euler, scale, color: optional inputs to Vizard
+                "set<X>" functions, to modify and display an object at the same time
+        """
+        if position is not None:
+            self._o[key].setPosition(position)
+        if scale is not None:
+            self._o[key].setScale(scale)
+        if euler is not None:
+            self._o[key].setEuler(euler)
+        if color is not None:
+            self._o[key].color(color)
+        self._o[key].visible(True)
+
+
+    def showOnly(self, key, position=None, euler=None, scale=None, color=None):
+        """ Show only the specified object from the collection,
+        hiding all other objects. Allows quickly setting position, 
+        scale, euler orientation and/or color in the same call.
+
+        Args:
+            key (str): Key of the object to show
+        position, euler, scale, color: optional inputs to Vizard
+            "set<X>" functions, to modify and display an object at the same time
+        """
+        self.hideAll()
+        self.show(key, position=position, euler=euler, scale=scale, color=color)
+
+
+    @classmethod
+    def fromFiles(cls, files):
+        """ Create a new ObjectCollection by loading assets from
+        a folder or list of file names. File base names will be used
+        as keys in the collection.
+
+        Args:
+            files: path spec, e.g. 'objects/*.glb', or list of file names
+        """
+        obj = {}
+        if type(files) == str:
+            for of in glob.glob(files):
+                key = os.path.splitext(os.path.split(of)[1])[0]
+                obj[key] = vizfx.addChild(of)
+                obj[key].visible(False)
+        else:
+            for of in files:
+                try:
+                    key = os.path.splitext(os.path.split(of)[1])[0]
+                    obj[key] = vizfx.addChild(of)
+                    obj[key].visible(False)
+                except:
+                    print('Error: could not load {:s}'.format(of))
+
+        return ObjectCollection(_src=obj)
+
+
 
 def showVRText(msg, color=[1.0, 1.0, 1.0], distance=2.0, scale=0.05, duration=3.0):
     """ Display head-locked message in VR, e.g. for instructions.
